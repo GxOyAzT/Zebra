49442933f47c4fdd65b4ec42ca1bfb60193129c4
Jakub Koza <jakub.koza13@gmail.com>
2021-10-21 23:46:31 +0200

    file manager v1

---
+++ Zebra.Shared.FileDriver/Features/Save/ISaveFile.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Zebra.Shared.FileDriver.Features.Save
+{
+    public interface ISaveFile
+    {
+        string Save(byte[] bytes, string directPath, string fileName);
+    }
+}
